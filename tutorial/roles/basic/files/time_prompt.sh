c39948323ecd9843d693961ec69eca630625dd93
lastnitescurry <brian.dinneen@gmail.com>
2018-09-09 12:11:55 +0100

    Start refactor out of tutorial / learning mode

--- tutorial/roles/basic/files/time_prompt.sh
+++ tutorial/roles/basic/files/time_prompt.sh
@@ -1,4 +1,5 @@
 #https://linoxide.com/how-tos/change-linux-shell-prompt-with-different-colors/
 
+# TODO Its probably not a good thing to defined this for none interactive shells
 #export PS1="[\\d \\t] \\u@\\h: \\w \\$ "
 export PS1="[\e[0;32m\d \t\e[m] \e[0;33m\u@\h\e[m: \w \$ "
