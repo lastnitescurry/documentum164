99e39177cf0f3042572b561e20de5a2a41a3798c
lastnitescurry <brian.dinneen@gmail.com>
2018-10-01 20:25:15 +0100

    Complete create database and unpack client software

--- roles/documentum_server/templates/serverSetup.sh
+++ roles/documentum_server/templates/serverSetup.sh
@@ -1,5 +1,5 @@
 export LC_ALL=C
-export HOME=/home/dmadmin
+export HOME=/home/{{ documentum_server.user }}
 export DOCUMENTUM={{ documentum_server.documentum }}
 export DOCUMENTUM_SHARED={{ documentum_server.dctm_shared }}
 export DM_HOME={{ documentum_server.dm_home }}
